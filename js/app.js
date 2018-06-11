283c6083f7a55204559815c49443357e792f0807
Admin <admin@admin.ad>
2018-06-11 11:40:21 +0300

    1ndC - Major fixes

--- js/app.js
+++ js/app.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
         };
         $mainSlider.slick(options);
     })();
+
+    //Кнопка Наверх
+    $(function () {
+        const optionToTopBtn = 2;
+        const showToTopBtnOn = document.documentElement.clientHeight * optionToTopBtn;
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > showToTopBtnOn) {
+                $('#toTop').fadeIn();
+            } else {
+                $('#toTop').fadeOut();
+            }
+        });
+
+        $('#toTop').click(function () {
+            $('body,html').animate({scrollTop: 0}, 800);
+        });
+
+    });
+
 });
